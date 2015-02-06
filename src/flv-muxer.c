869680dcc4bfea70a9a1930bfb67dd198745f906
Akagi201 <akagi201@gmail.com>
2015-02-06 17:10:16 +0800

    fix avcc tag

--- src/flv-muxer.c
+++ src/flv-muxer.c
@@ -52,7 +52,7 @@ void write_flv_header(bool is_have_audio, bool is_have_video) {
 }
 
 /*
- * @brief
+ * @brief write video tag
  * @param[in] buf:
  * @param[in] buf_len: flv tag body size
  * @param[in] timestamp: flv tag timestamp
@@ -81,11 +81,16 @@ void write_video_tag(uint8_t *buf, uint32_t buf_len, uint32_t timestamp) {
 
 }
 
+/*
+ * @brief write header of video tag data part, fixed 5 bytes
+ *
+ */
 void write_avc_data_tag(const uint8_t *data, uint32_t data_len, uint32_t timestamp, int is_keyframe) {
     uint8_t *buf = (uint8_t *) malloc(data_len + 5);
     uint8_t *pbuf = buf;
 
-    unsigned char flag = 0;
+    uint8_t flag = 0;
+    // (FrameType << 4) | CodecID, 1 - keyframe, 2 - inner frame, 7 - AVC(h264)
     if (is_keyframe) {
         flag = 0x17;
     } else {
@@ -94,7 +99,7 @@ void write_avc_data_tag(const uint8_t *data, uint32_t data_len, uint32_t timesta
 
     pbuf = ui08_to_bytes(pbuf, flag);
 
-    pbuf = ui08_to_bytes(pbuf, 1);    // avc packet type (0, nalu)
+    pbuf = ui08_to_bytes(pbuf, 1);    // AVCPacketType: 0x00 - AVC sequence header; 0x01 - AVC NALU
     pbuf = ui24_to_bytes(pbuf, 0);    // composition time
 
     memcpy(pbuf, data, data_len);
@@ -127,8 +132,11 @@ void write_video_data_tag(const uint8_t *data, uint32_t data_len, uint32_t times
     write_video_tag((uint8_t *) data, data_len, (uint32_t) (timestamp - g_time_begin));
 }
 
+/*
+ * @brief write AVC sequence header in header of video tag data part, the first video tag
+ */
 void write_avc_sequence_header_tag(const uint8_t *sps, uint32_t sps_len, const uint8_t *pps, uint32_t pps_len) {
-    uint8_t avc_seq_buf[4096] = {0};
+    uint8_t avc_seq_buf[4096] = {0}; // TODO: total AVCC length 11 + sps_len + pps_len, change to use malloc
     uint8_t *pbuf = avc_seq_buf;
 
     uint8_t flag = 0;
@@ -138,27 +146,34 @@ void write_avc_sequence_header_tag(const uint8_t *sps, uint32_t sps_len, const u
 
     pbuf = ui08_to_bytes(pbuf, flag);
 
-    pbuf = ui08_to_bytes(pbuf, 0); // avc packet type (0, header)
+    pbuf = ui08_to_bytes(pbuf, 0); // AVCPacketType: 0x00 - AVC sequence header
     pbuf = ui24_to_bytes(pbuf, 0); // composition time
 
-    // AVCDecoderConfigurationRecord
+    // generate AVCC with sps and pps, AVCDecoderConfigurationRecord
 
     pbuf = ui08_to_bytes(pbuf, 1); // configurationVersion
     pbuf = ui08_to_bytes(pbuf, sps[1]); // AVCProfileIndication
     pbuf = ui08_to_bytes(pbuf, sps[2]); // profile_compatibility
     pbuf = ui08_to_bytes(pbuf, sps[3]); // AVCLevelIndication
-    pbuf = ui08_to_bytes(pbuf, 0xff); // 6 bits reserved (111111) + 2 bits nal size length - 1
-    pbuf = ui08_to_bytes(pbuf, (uint16_t) sps_len); // sps
+    // 6 bits reserved (111111) + 2 bits nal size length - 1
+    // (Reserved << 2) | Nal_Size_length = (0x3F << 2) | 0x03 = 0xFF
+    pbuf = ui08_to_bytes(pbuf, 0xff);
+    // 3 bits reserved (111) + 5 bits number of sps (00001)
+    // (Reserved << 5) | Number_of_SPS = (0x07 << 5) | 0x01 = 0xe1
+    pbuf = ui08_to_bytes(pbuf, 0xe1);
 
+    // sps
+    pbuf = ui16_to_bytes(pbuf, (uint16_t) sps_len);
     memcpy(pbuf, sps, sps_len);
-
     pbuf += sps_len;
+
+    // pps
     pbuf = ui08_to_bytes(pbuf, 1); // number of pps
-    pbuf = ui16_to_bytes(pbuf, (uint16_t) pps); // pps
+    pbuf = ui16_to_bytes(pbuf, (uint16_t) pps);
     memcpy(pbuf, pps, pps_len);
     pbuf += pps_len;
 
-    write_video_data_tag(avc_seq_buf, (uint32_t) (pbuf - avc_seq_buf), 0);
+    write_video_tag(avc_seq_buf, (uint32_t) (pbuf - avc_seq_buf), 0);
 
     return;
 }
